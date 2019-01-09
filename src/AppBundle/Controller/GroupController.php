0fc3a39e202e25126b12b557cb2bdebfce1a600b
Hossam Nageh <Manchister@ymail.com>
2019-01-09 23:19:06 +0200

    fix delete group

--- src/AppBundle/Controller/GroupController.php
+++ src/AppBundle/Controller/GroupController.php
@@ -41,6 +41,49 @@ class GroupController extends Controller
         ->getForm();
 
         // Handle Request
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Groups;
+use AppBundle\Entity\Users;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class GroupController extends Controller
+{
+    /**
+     * @Route("/groups", name="groups_list")
+     */
+    public function indexAction(Request $request)
+    {
+        $groups = $this->getDoctrine()->getRepository('AppBundle:Groups')->findAll();
+
+        // Render Template
+        return $this->render('group/index.html.twig', array(
+            'groups' => $groups,
+        ));
+    }
+
+    /**
+     * @Route("/groups/create", name="group_create")
+     */
+    public function createAction(Request $request)
+    {
+        $group = new Groups;
+
+        $form = $this->createFormBuilder($group)
+        ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px;')))
+        ->add('save', SubmitType::class, array('label' => 'Create Group', 'attr' => array('class' => 'btn btn-success')))
+        ->getForm();
+
+        // Handle Request
         $form->handleRequest($request);
 
         // Check Submit
@@ -107,6 +150,59 @@ class GroupController extends Controller
         return $this->render('group/edit.html.twig', array(
             'form' => $form->createView()
         ));
+
+    }
+
+
+
+    /**
+     * @Route("/groups/delete/{id}", name="group_delete")
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $group = $em->getRepository('AppBundle:Groups')->find($id);
+        $users = $this->getDoctrine()->getRepository('AppBundle:Users')->findBy(
+            ['userGroup' => $id]
+        );
+
+        if (!$group) {
+            throw $this->createNotFoundException('No group found for id ' . $id);
+        }
+        // Check if group includes users
+        if ($users) {
+            $this->addFlash('error', 'Group includes users, can not delete');
+            return $this->redirectToRoute('groups_list');
+        } else {
+            $em->remove($group);
+            $em->flush();
+            $this->addFlash('notice', 'Group Deleted');
+            return $this->redirectToRoute('groups_list');
+        }
+
+        
+
+        
+    }
+
+
+
+    /**
+     * @Route("/groups/{id}/", name="group_users")
+     */
+    public function showUsersAction($id, Request $request)
+    {
+        $users = $this->getDoctrine()->getRepository('AppBundle:Users')->findBy(
+            ['userGroup' => $id]
+        );
+        // $group = $this->getDoctrine()->getRepository('AppBundle:Groups')->find($id);
+
+        // Render Template
+        return $this->render('group/users.html.twig', array(
+            'users' => $users,
+        ));
+    }
+}
 
     }
 
